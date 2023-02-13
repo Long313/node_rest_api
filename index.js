eabe5ff733937d62df91437791e0d626cec09c6b
Long Tran <tranxuanlonga555@gmail.com>
2023-02-13 08:35:52 +0700

    rest api post uset

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:false},(err)=> {
     // console.log("Connected to MongoDB");
-    if(err) console.log("err::",err)
+    if(err) console.log("err:",err)
     else console.log("mongoDB is connected");
 });
 
@@ -21,6 +22,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
+app.use("/api/posts",postRoute);
 
 app.get("/",(req,res) => {
     res.send("welcome to homepage");
